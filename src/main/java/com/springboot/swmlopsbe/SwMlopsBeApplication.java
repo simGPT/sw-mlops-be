0116953f8aac2ae9db82b3bfb0877b89d45d6f38
simGPT <simGPT02@gmail.com>
2026-06-08 18:15:57 +0900

    :tada: sw-mlops-be 프로젝트 시작

---
+++ src/main/java/com/springboot/swmlopsbe/SwMlopsBeApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.swmlopsbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwMlopsBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwMlopsBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springboot/swmlopsbe/SwMlopsBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.swmlopsbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwMlopsBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
