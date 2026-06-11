4963a0d0af9e10da1d44202f40f3c29f91ef5c1f
simGPT <simGPT02@gmail.com>
2026-06-11 16:40:47 +0900

    :sparkles:Feat: churn/uplift api 입력값 계산 쿼리 추가

--- src/main/java/com/springboot/swmlopsbe/domain/log/repository/CustomerLogRepository.java
+++ src/main/java/com/springboot/swmlopsbe/domain/log/repository/CustomerLogRepository.java
@@ -1,7 +1,22 @@
 package com.springboot.swmlopsbe.domain.log.repository;
 
+import java.time.LocalDateTime;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.springboot.swmlopsbe.domain.log.entity.CustomerLog;
+import com.springboot.swmlopsbe.domain.log.entity.EventType;
+import com.springboot.swmlopsbe.domain.user.entity.User;
 
-public interface CustomerLogRepository extends JpaRepository<CustomerLog, Long> {}
+public interface CustomerLogRepository extends JpaRepository<CustomerLog, Long> {
+
+    // 사용자 행동 로그 개수
+  @Query(
+      "SELECT COUNT(l) FROM CustomerLog l WHERE l.user = :user AND l.eventType = :eventType AND l.createdAt >= :since")
+  long countByUserAndEventTypeAndCreatedAtAfter(
+      @Param("user") User user,
+      @Param("eventType") EventType eventType,
+      @Param("since") LocalDateTime since);
+}
